1fa7b745a5c8f3dcbc2bbc6381fba73bf7bfa10e
David Fowler <davidfowl@gmail.com>
2014-03-28 16:05:44 -0700

    Use ILibraryManager to find assemblies with hubs - Remove dependency on AppDomain

--- src/Microsoft.AspNet.SignalR.Server/Hubs/DefaultAssemblyLocator.cs
+++ src/Microsoft.AspNet.SignalR.Server/Hubs/DefaultAssemblyLocator.cs
@@ -1,16 +1,28 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
 
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Reflection;
+using Microsoft.Net.Runtime;
 
 namespace Microsoft.AspNet.SignalR.Hubs
 {
     public class DefaultAssemblyLocator : IAssemblyLocator
     {
+        private readonly ILibraryManager _libraryManager;
+
+        public DefaultAssemblyLocator(ILibraryManager libraryManager)
+        {
+            _libraryManager = libraryManager;
+        }
+
         public virtual IList<Assembly> GetAssemblies()
         {
-            return AppDomain.CurrentDomain.GetAssemblies();
+            var libraries = _libraryManager.GetReferencingLibraries(typeof(Hub).GetTypeInfo().Assembly.GetName().Name);
+
+            return libraries.Select(library => Assembly.Load(new AssemblyName(library.Name)))
+                            .ToList();
         }
     }
 }
