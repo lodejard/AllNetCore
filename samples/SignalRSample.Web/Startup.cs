c848778f1d719c715b442ed806b6ffac03c3f432
David Fowler <davidfowl@gmail.com>
2014-07-06 21:50:52 -0700

    Started porting sample

--- samples/SignalRSample.Web/Startup.cs
+++ samples/SignalRSample.Web/Startup.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.SignalR;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
 
 namespace SignalRSample.Web
@@ -20,27 +18,9 @@ namespace SignalRSample.Web
             });
 
             app.UseFileServer();
+
+            app.UseSignalR<RawConnection>("/raw-connection");
             app.UseSignalR();
-        }
-    }
-
-    public class Chat : Hub
-    {
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            Clients.All.send(Context.ConnectionId + " disconnected" + (stopCalled ? "" : " after timeout"));
-            return base.OnDisconnected(stopCalled);
-        }
-
-        public override Task OnConnected()
-        {
-            Clients.All.send(Context.ConnectionId + " connected");
-            return base.OnConnected();
-        }
-
-        public void Send(string message)
-        {
-            Clients.All.send(message);
         }
     }
 }
