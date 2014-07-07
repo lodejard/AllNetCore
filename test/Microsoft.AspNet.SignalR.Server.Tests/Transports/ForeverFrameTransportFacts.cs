66c1a4552e324d3b07edda4a1f82f2933705e30b
David Fowler <davidfowl@gmail.com>
2014-07-06 20:34:13 -0700

    Added ForeverFrameTransportEscapesTags - Skipped tests that assume regular ajax sends  would be escaped temporarily.

---
+++ test/Microsoft.AspNet.SignalR.Server.Tests/Transports/ForeverFrameTransportFacts.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Messaging;
+using Microsoft.AspNet.SignalR.Transports;
+using Microsoft.Framework.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.Tests
+{
+    public class ForeverFrameTransportFacts
+    {
+        [Theory(Skip = "Should we escape regular sends?")]
+        [InlineData("</sCRiPT>", "\\u003c/sCRiPT\\u003e")]
+        [InlineData("</SCRIPT dosomething='false'>", "\\u003c/SCRIPT dosomething='false'\\u003e")]
+        [InlineData("<p>ELLO</p>", "\\u003cp\\u003eELLO\\u003c/p\\u003e")]
+        public void ForeverFrameTransportEscapesTags(string data, string expected)
+        {
+            var context = new TestContext("/");
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+
+            var ms = new MemoryStream();
+            context.MockResponse.Setup(m => m.Body).Returns(ms);
+
+            var fft = ta.CreateInstance<ForeverFrameTransport>(sp, context.MockHttpContext.Object);
+
+            AssertEscaped(fft, ms, data, expected);
+        }
+
+        [Theory]
+        [InlineData("<script type=\"\"></script>", "\\u003cscript type=\"\"\\u003e\\u003c/script\\u003e")]
+        [InlineData("<script type=''></script>", "\\u003cscript type=''\\u003e\\u003c/script\\u003e")]
+        public void ForeverFrameTransportEscapesTagsWithPersistentResponse(string data, string expected)
+        {
+            var context = new TestContext("/");
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+
+            var ms = new MemoryStream();
+            context.MockResponse.Setup(m => m.Body).Returns(ms);
+
+            var fft = ta.CreateInstance<ForeverFrameTransport>(sp, context.MockHttpContext.Object);
+            fft.ConnectionId = "1";
+
+            AssertEscaped(fft, ms, GetWrappedResponse(data), expected);
+        }
+
+        [Theory]
+        [InlineData("invalid")]
+        [InlineData("-100")]
+        [InlineData("1,000")]
+        [InlineData(" ")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ForeverFrameTransportThrowsOnInvalidFrameId(string frameId)
+        {
+            var qs = new Dictionary<string, string> { { "frameId", frameId } };
+            var context = new TestContext("/", qs);
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+
+            var ms = new MemoryStream();
+            context.MockResponse.Setup(m => m.Body).Returns(ms);
+
+            var fft = ta.CreateInstance<ForeverFrameTransport>(sp, context.MockHttpContext.Object);
+            var connection = new Mock<ITransportConnection>();
+
+            Assert.Throws(typeof(InvalidOperationException), () => fft.InitializeResponse(connection.Object));
+        }
+
+        [Fact]
+        public void ForeverFrameTransportSetsCorrectContentType()
+        {
+            var qs = new Dictionary<string, string> {
+                { "frameId", "1" }
+            };
+            var context = new TestContext("/", qs);
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+
+            var ms = new MemoryStream();
+            context.MockResponse.SetupAllProperties();
+            context.MockResponse.Setup(m => m.Body).Returns(ms);
+
+            var fft = ta.CreateInstance<ForeverFrameTransport>(sp, context.MockHttpContext.Object);
+            fft.ConnectionId = "1";
+            var connection = new Mock<ITransportConnection>();
+
+            fft.InitializeResponse(connection.Object).Wait();
+
+            Assert.Equal("text/html; charset=UTF-8", context.MockResponse.Object.ContentType);
+        }
+
+        private static void AssertEscaped(ForeverFrameTransport fft, MemoryStream ms, object input, string expectedOutput)
+        {
+            fft.Send(input).Wait();
+
+            string rawResponse = Encoding.UTF8.GetString(ms.ToArray());
+
+            // Doing contains due to all the stuff that gets sent through the buffer
+            Assert.True(rawResponse.Contains(expectedOutput));
+        }
+
+        private static void AssertEscaped(ForeverFrameTransport fft, MemoryStream ms, PersistentResponse input, string expectedOutput)
+        {
+            fft.Send(input).Wait();
+
+            string rawResponse = Encoding.UTF8.GetString(ms.ToArray());
+
+            // Doing contains due to all the stuff that gets sent through the buffer
+            Assert.True(rawResponse.Contains(expectedOutput));
+        }
+
+        private static PersistentResponse GetWrappedResponse(string raw)
+        {
+            var data = Encoding.Default.GetBytes(raw);
+            var message = new Message("foo", "key", new ArraySegment<byte>(data));
+
+            var response = new PersistentResponse
+            {
+                Messages = new List<ArraySegment<Message>>
+                {
+                    new ArraySegment<Message>(new Message[] { message })
+                }
+            };
+
+            return response;
+        }
+    }
+}
