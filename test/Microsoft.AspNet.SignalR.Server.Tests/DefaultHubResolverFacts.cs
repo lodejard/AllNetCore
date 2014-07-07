a43ab3b335f1607be85537cf1f64e4e8d2e6ff35
David Fowler <davidfowl@gmail.com>
2014-07-06 19:26:48 -0700

    Added DefaultActionResolverFacts, DefaultHubResolverFacts and GroupManagerFacts

---
+++ test/Microsoft.AspNet.SignalR.Server.Tests/DefaultHubResolverFacts.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Framework.DependencyInjection;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.Tests
+{
+    public class DefaultHubResolverFacts
+    {
+        [Fact]
+        public void ShouldResolveHubByTypeName()
+        {
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+            var hubResolver = ta.CreateInstance<ReflectedHubDescriptorProvider>(sp);
+            HubDescriptor hub;
+            hubResolver.TryGetHub("HubWithoutAttribute", out hub);
+
+            Assert.NotNull(hub);
+            Assert.Equal(hub.Name, "HubWithoutAttribute");
+            Assert.Equal(hub.NameSpecified, false);
+        }
+
+        [Fact]
+        public void ShouldResolveHubByHubNameAttribute()
+        {
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+            var hubResolver = ta.CreateInstance<ReflectedHubDescriptorProvider>(sp);
+            HubDescriptor hub;
+            hubResolver.TryGetHub("NameFromAttribute", out hub);
+
+            Assert.NotNull(hub);
+            Assert.Equal(hub.Name, "NameFromAttribute");
+            Assert.Equal(hub.NameSpecified, true);
+        }
+
+        [Fact]
+        public void ShouldNotResolveHubByFullTypeName()
+        {
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+            var hubResolver = ta.CreateInstance<ReflectedHubDescriptorProvider>(sp);
+            HubDescriptor hub;
+            hubResolver.TryGetHub("SignalR.Tests.HubWithoutAttribute", out hub);
+
+            Assert.Null(hub);
+        }
+
+        [Fact]
+        public void ShouldNotResolveHubByTypeNameIfAttributeExists()
+        {
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+            var hubResolver = ta.CreateInstance<ReflectedHubDescriptorProvider>(sp);
+            HubDescriptor hub;
+            hubResolver.TryGetHub("HubWithAttribute", out hub);
+
+            Assert.Null(hub);
+        }
+
+        [Fact]
+        public void ShouldIgnoreCaseWhenDiscoveringHubs()
+        {
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+            var hubResolver = ta.CreateInstance<ReflectedHubDescriptorProvider>(sp);
+            HubDescriptor hub;
+            hubResolver.TryGetHub("hubwithoutattribute", out hub);
+
+            Assert.NotNull(hub);
+            Assert.Equal(hub.Name, "HubWithoutAttribute");
+            Assert.Equal(hub.NameSpecified, false);
+        }
+
+        [Fact]
+        public void ShouldIgnoreCaseWhenDiscoveringHubsUsingManager()
+        {
+            var sp = ServiceProviderHelper.CreateServiceProvider();
+            var ta = new TypeActivator();
+            var manager = ta.CreateInstance<DefaultHubManager>(sp);
+            var hub = manager.GetHub("hubwithoutattribute");
+
+            Assert.NotNull(hub);
+            Assert.Equal(hub.Name, "HubWithoutAttribute");
+            Assert.Equal(hub.NameSpecified, false);
+        }
+
+        [HubName("NameFromAttribute")]
+        public class HubWithAttribute : Hub
+        {
+        }
+
+        public class HubWithoutAttribute : Hub
+        {
+        }
+    }
+}
