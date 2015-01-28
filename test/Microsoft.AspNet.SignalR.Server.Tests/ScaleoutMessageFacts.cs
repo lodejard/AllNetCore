2a3778d65fa8b46b0b6ae23ab9984e282eb5c4e3
Brennan <brecon@microsoft.com>
2015-01-28 09:01:55 -0800

    Scaleout Message Test

---
+++ test/Microsoft.AspNet.SignalR.Server.Tests/ScaleoutMessageFacts.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR.Messaging;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.Server.Tests
+{
+    public class ScaleoutMessageFacts
+    {
+        [Fact]
+        void FromBytesToBytesProducesCorrectValues()
+        {
+            var message = new Message("source", "key", "value");
+            var message2 = new Message("string", "more", "str");
+            var message3 = new Message("s", "l", "n");
+            var scaleoutMessage = new ScaleoutMessage(new List<Message>() { message, message2, message3 });
+
+            var bytes = scaleoutMessage.ToBytes();
+            var msg = ScaleoutMessage.FromBytes(bytes);
+
+            Assert.True(scaleoutMessage.Messages.Count == 3);
+            Assert.True(scaleoutMessage.Messages[0].Source == msg.Messages[0].Source, "Source is not the same");
+            Assert.True(scaleoutMessage.Messages[0].Key == msg.Messages[0].Key, "Key is not the same");
+            Assert.True(scaleoutMessage.Messages[0].GetString() == msg.Messages[0].GetString(), "Value is not the same");
+        }
+    }
+}
