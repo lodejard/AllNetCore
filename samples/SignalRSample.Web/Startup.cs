e4bfac5ecbe465cdb1d3e99d21b2a99f0de2cd0c
Pranav K <prkrishn@hotmail.com>
2015-05-27 18:11:01 -0700

    Removing Serilog from sample

--- samples/SignalRSample.Web/Startup.cs
+++ samples/SignalRSample.Web/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNet.SignalR;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Serilog;
 
 namespace SignalRSample.Web
 {
@@ -19,16 +18,6 @@ namespace SignalRSample.Web
 
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-#if DNX451
-            string OutputTemplate = "{SourceContext} {Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}";
-
-            var serilog = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .WriteTo.RollingFile(@".\SignalR-Log-{Date}.txt", outputTemplate: OutputTemplate);
-
-            loggerFactory.AddSerilog(serilog);
-#endif
-
             app.UseFileServer();
 
             app.UseSignalR<RawConnection>("/raw-connection");
