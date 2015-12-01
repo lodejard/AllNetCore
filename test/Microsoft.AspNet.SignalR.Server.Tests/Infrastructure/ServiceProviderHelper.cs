staff0rd/SignalR-Server

reacting to new IServer IHttpApplication design

test/Microsoft.AspNet.SignalR.Server.Tests/Infrastructure/ServiceProviderHelper.cs
@@ -36,7 +36,7 @@ namespace Microsoft.AspNet.SignalR.Tests
         {
             IFeatureCollection IServer.Features { get; }
             
-            public void Start(RequestDelegate requestDelegate)
+            public void Start<THttpContext>(IHttpApplication<THttpContext> application)
             {
 
             }

lodejard/AllNetCore

test/Microsoft.AspNet.SignalR.Server.Tests/Infrastructure/ServiceProviderHelper.cs
@@ -36,7 +36,7 @@ namespace Microsoft.AspNet.SignalR.Tests
         {
             IFeatureCollection IServer.Features { get; }
             
-            public void Start(RequestDelegate requestDelegate)
+            public void Start<TContext>(IHttpApplication<TContext> application)
             {
 
             }